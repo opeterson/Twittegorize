0a4ba41c67113b1edc2e55de1cee098303ff93e5
Owen Peterson <owen.a.peterson@gmail.com>
2015-03-26 08:38:08 -0500

    Added the interface used to take action when the call to twitter for the specific tweet is finished.

---
+++ app/src/main/java/ca/owenpeterson/twittegorize/utils/OnDetailTweetLoaded.java
@@ -0,0 +1,11 @@
+package ca.owenpeterson.twittegorize.utils;
+
+import ca.owenpeterson.twittegorize.models.DetailTweet;
+
+/**
+ * Created by Owen on 3/25/2015.
+ */
+public interface OnDetailTweetLoaded {
+
+    public void onDetailTweetLoaded(DetailTweet detailTweet);
+}
