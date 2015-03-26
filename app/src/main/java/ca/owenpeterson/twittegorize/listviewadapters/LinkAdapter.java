e8f13c87eb4570af85275c483ac9f0aa34f53cf3
Owen Peterson <owen.a.peterson@gmail.com>
2015-03-26 14:29:51 -0500

    Added the components for a listview of URLS on the TweetDetailsActivity

---
+++ app/src/main/java/ca/owenpeterson/twittegorize/listviewadapters/LinkAdapter.java
@@ -0,0 +1,42 @@
+package ca.owenpeterson.twittegorize.listviewadapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.net.URL;
+import java.util.List;
+
+import ca.owenpeterson.twittegorize.R;
+
+/**
+ * Created by Owen on 3/26/2015.
+ */
+public class LinkAdapter extends ArrayAdapter<URL> {
+
+    private TextView link;
+
+    public LinkAdapter(Context context, List<URL> links) {
+        super(context, 0, links);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+
+        if (null == view) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.plain_url_item, null);
+        }
+
+        link = (TextView) view.findViewById(R.id.text_details_url_item);
+        URL url = getItem(position);
+        String text = url.toString();
+        link.setText(text);
+
+        return view;
+    }
+}
