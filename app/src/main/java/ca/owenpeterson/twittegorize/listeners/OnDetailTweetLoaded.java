b4376c64c2ffd5996d4ec52e7b3f699067d65d0d
Owen Peterson <owen.a.peterson@gmail.com>
2015-04-14 19:27:49 -0500

    More docs!

--- app/src/main/java/ca/owenpeterson/twittegorize/listeners/OnDetailTweetLoaded.java
+++ app/src/main/java/ca/owenpeterson/twittegorize/listeners/OnDetailTweetLoaded.java
@@ -4,6 +4,10 @@ import ca.owenpeterson.twittegorize.models.DetailTweet;
 
 /**
  * Created by Owen on 3/25/2015.
+ *
+ * Used as a callback when opening the tweet details activity. When the user clicks on a tweet in
+ * the list, a call is made to twitter to get the one tweet they clicked on. This interface is
+ * used when the app receives a response from twitter.
  */
 public interface OnDetailTweetLoaded {
 
