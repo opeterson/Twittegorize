2755310324b0a2c7c8afe394be1e12be8e071641
opeterson <owen.a.peterson@gmail.com>
2015-07-14 18:48:12 -0500

    Created TweetDAO class and copied persistence methods into the new class.

---
+++ app/src/main/java/ca/owenpeterson/twittegorize/data/TweetDAO.java
@@ -0,0 +1,47 @@
+package ca.owenpeterson.twittegorize.data;
+
+import com.activeandroid.query.Select;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import ca.owenpeterson.twittegorize.models.Tweet;
+import ca.owenpeterson.twittegorize.models.TweetComparator;
+
+/**
+ * Created by owen on 7/14/15.
+ */
+public class TweetDAO {
+
+    private TweetComparator comparator = new TweetComparator();
+
+    public TweetDAO(){}
+
+    public List<Tweet> getAllTweets() {
+        List<Tweet> tweets = new Select().from(Tweet.class).orderBy("tweetId").execute();
+
+        Collections.sort(tweets, comparator);
+        return tweets;
+    }
+
+    public List<Tweet> getTweetsByCategoryId(long categoryId) {
+        TwitterUserManager userManager = new TwitterUserManager();
+        List<Long> userIdsForCategory = userManager.getUserIdsInCategory(categoryId);
+
+        Object[] idsArray = userIdsForCategory.toArray();
+        String userIds = Arrays.toString(idsArray);
+        userIds = userIds.replace("[", "");
+        userIds = userIds.replace("]", "");
+
+        List<Tweet> tweets = new Select().from(Tweet.class).where("User IN (" + userIds + ")").orderBy("tweetId DESC").execute();
+
+        return tweets;
+
+    }
+
+    public Tweet getLatestTweet() {
+        Tweet latestTweet = new Select().from(Tweet.class).orderBy("Id DESC").limit(1).executeSingle();
+        return latestTweet;
+    }
+}
