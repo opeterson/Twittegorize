57b1797e0cf88a748b925a2acdd700626f470ede
opeterson <owen.a.peterson@gmail.com>
2015-08-03 09:04:37 -0500

    Added DAO method to delete a list of RetweetedUsers

--- app/src/main/java/ca/owenpeterson/twittegorize/data/RetweetedUserDAO.java
+++ app/src/main/java/ca/owenpeterson/twittegorize/data/RetweetedUserDAO.java
@@ -35,6 +35,24 @@ public class RetweetedUserDAO {
         return success;
     }
 
+    public boolean deleteRetweetedUserList(List<RetweetedUser> retweetedUsers) {
+        boolean success = false;
+
+        ActiveAndroid.beginTransaction();
+        try {
+            for (RetweetedUser ru : retweetedUsers) {
+                ru.delete();
+            }
+            ActiveAndroid.setTransactionSuccessful();
+            success = true;
+        }
+        finally {
+            ActiveAndroid.endTransaction();
+        }
+
+        return success;
+    }
+
     public List<RetweetedUser> getRetweetedUsersOlderThanDate(DateTime olderThanDate) {
         Long dateInMillis = olderThanDate.getMillis();
         return new Select().from(RetweetedUser.class).where("createdDate <= " + dateInMillis).execute();
